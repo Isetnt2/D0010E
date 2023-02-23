5e10872d80b622692f14bcb32681089d65539ef6
isetnt2 <31891931+Isetnt2@users.noreply.github.com>
2023-02-23 08:35:31 +0100

    Initial commit of labb4

---
+++ D0010E/src/labb4/src/Graph.java
@@ -0,0 +1,5 @@
+public interface Graph {
+    public void Graph();
+    abstract void addNode(int id, int x, int y);
+    abstract void addEdge(int id1, int id2, int weight);
+}
---
+++ D0010E/src/labb4/src/Queue.java
@@ -0,0 +1,4 @@
+public interface Queue {
+    abstract void Queue();
+
+}
